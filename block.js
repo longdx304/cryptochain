f82d10f5e66508e7ad20653f718b217e66880f78
Long Do <longdo@dobtech.vn>
2022-02-02 16:21:54 +0700

    add mineBlock function

--- block.js
+++ block.js
@@ -1,8 +1,22 @@
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 class Block {
   static genesis() {
     return new this(GENESIS_DATA);
+  }
+
+  static mineBlock({ lastBlock, data }) {
+    const lastHash = lastBlock.hash;
+    const timestamp = Date.now();
+    const hash = cryptoHash(lastHash, timestamp, data);
+
+    return new this({
+      lastHash,
+      data,
+      timestamp,
+      hash,
+    });
   }
 
   constructor({ timestamp, lastHash, hash, data }) {
