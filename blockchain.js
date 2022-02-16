11786172bdc0ad108dc089ecadba57268a2bb62e
Long Do <longdo@longs-mbp.lan>
2022-02-16 16:02:13 +0700

    add chain validate function

--- blockchain.js
+++ blockchain.js
@@ -1,7 +1,30 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
   chain = [Block.genesis()];
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const { lastHash, timestamp, data, hash } = chain[i];
+      const actualLastHash = chain[i - 1].hash;
+
+      if (lastHash !== actualLastHash) {
+        return false;
+      }
+
+      const validatedHash = cryptoHash(lastHash, timestamp, data);
+      if (validatedHash !== hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 
   addBlock({ data }) {
     const lastBlock = this.chain[this.chain.length - 1];
